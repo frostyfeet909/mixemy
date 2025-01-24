356fd9d047a866866d685358433958cff0fc914b
frostyfeet909 <algiesampson@gmail.com>
2025-01-24 02:30:00 +0000

    Table Naming

--- src/mixemy/models/_base.py
+++ src/mixemy/models/_base.py
@@ -8,7 +8,10 @@ class BaseModel(DeclarativeBase):
     @declared_attr  # pyright: ignore[reportArgumentType]
     @classmethod
     def __tablename__(cls) -> str:
-        return cls.__name__.lower()
+        return "".join(
+            (letter if letter.islower() else "_" + letter.lower())
+            for letter in (cls.__name__[0].lower() + cls.__name__[1:])
+        ).removesuffix("_model")
 
     def __repr__(self) -> str:
         return f"<{self.__class__.__name__} {self.__dict__}>"
