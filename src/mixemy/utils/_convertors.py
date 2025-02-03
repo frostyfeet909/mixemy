bd9aebaaf7b9d7f96a62d8eb121fd0da9acbe8df
frostyfeet909 <algiesampson@gmail.com>
2025-02-03 23:45:23 +0000

    Enable serialization via alias

--- src/mixemy/utils/_convertors.py
+++ src/mixemy/utils/_convertors.py
@@ -7,10 +7,14 @@ from mixemy.types import BaseModelT, BaseSchemaT
 
 def unpack_schema(
     schema: BaseSchema,
+    *,
     exclude_unset: bool = True,
     exclude: set[str] | None = None,
+    by_alias: bool = True,
 ) -> dict[str, Any]:
-    return schema.model_dump(exclude_unset=exclude_unset, exclude=exclude)
+    return schema.model_dump(
+        exclude_unset=exclude_unset, exclude=exclude, by_alias=by_alias
+    )
 
 
 def to_model(model: type[BaseModelT], schema: BaseSchema) -> BaseModelT:
